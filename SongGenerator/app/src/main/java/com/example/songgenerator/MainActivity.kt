8a99c6eeb77e8090442be00f6f6e451e862b8021
Kristoffer Borg Nilsen <kristofferbn98@gmail.com>
2019-09-22 18:45:38 +0200

    sliding bar working

--- SongGenerator/app/src/main/java/com/example/songgenerator/MainActivity.kt
+++ SongGenerator/app/src/main/java/com/example/songgenerator/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.songgenerator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var seekBarElement = findViewById<SeekBar>(R.id.seekBar)
+        var verses = findViewById<TextView>(R.id.number_view)
+        seekBarElement.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar, i: Int, b: Boolean) {
+                verses.text = i.toString()
+
+            }
+        })
     }
 }
